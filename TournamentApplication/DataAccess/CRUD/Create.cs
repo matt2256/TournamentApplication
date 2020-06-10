8fb719f138895c98c221dc8f2eef5b6e02127656
matt2256 <mads@mmm-m.dk>
2020-06-10 13:14:02 +0200

    Added CRUD classes to DataAccess

---
+++ TournamentApplication/DataAccess/CRUD/Create.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace DataAccess.CRUD
+{
+    public class Create
+    {
+        /// <summary>
+        /// Represent a overloadet method used for Create in CRUD 
+        /// </summary>
+        /// <param name="x">X represent a object. Where object is a <TournamentModel></param>
+        public void Creation(object x)
+        {
+            ///Represent a obejct of TournamentEntities
+            TournamentEntities Context = new TournamentEntities();
+        }
+
+    }
+}
