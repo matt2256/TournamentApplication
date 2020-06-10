3d91720a43440505aab95e7ff51cef11b65000b3
matt2256 <mads@mmm-m.dk>
2020-06-10 13:55:00 +0200

    Adding empty custom methods for Create (CRUD)

--- TournamentApplication/DataAccess/CRUD/Create.cs
+++ TournamentApplication/DataAccess/CRUD/Create.cs
@@ -9,14 +9,83 @@ namespace DataAccess.CRUD
 {
     public class Create
     {
+        TournamentEntities context = new TournamentEntities();
+
         /// <summary>
-        /// Represent a overloadet method used for Create in CRUD 
+        /// Represent method for creating matchup data 
         /// </summary>
-        /// <param name="x">X represent a object. Where object is a <TournamentModel></param>
-        public void Creation(object x)
+        public void Creation()
         {
-            ///Represent a obejct of TournamentEntities
-            TournamentEntities Context = new TournamentEntities();
+            Matchup matchup = new Matchup
+            {
+
+            };
+        }
+
+        /// <summary>
+        /// Represent a method for creating MatchupEntries data
+        /// </summary>
+        public void CreateMatchupEntries()
+        {
+            MatchupEntries matchupEntries = new MatchupEntries
+            {
+
+            };
+        }
+
+        /// <summary>
+        /// Represent a method for creating People Data
+        /// </summary>
+        public void CreatePeople()
+        {
+            People people = new People()
+            {
+
+            };
+        }
+
+        /// <summary>
+        /// Represent a method for creating TeamMembers data
+        /// </summary>
+        public void CreateTeamMembers()
+        {
+            TeamMembers teamMembers = new TeamMembers()
+            {
+
+            };
+        }
+
+        /// <summary>
+        /// Represent a method for creating Teams data
+        /// </summary>
+        public void CreateTeams()
+        {
+            Teams teams = new Teams
+            {
+
+            };
+        }
+
+        /// <summary>
+        /// Represent a method for creating TournamentsEntries data 
+        /// </summary>
+        public void CreateTournamentEntries()
+        {
+            TournamentEntries tournamentEntries = new TournamentEntries
+            {
+
+            };
+        }
+
+        /// <summary>
+        /// Represent a method for creating Tournaments
+        /// </summary>
+        public void CreateTournaments()
+        {
+            Tournaments tournaments = new Tournaments
+            {
+
+            };
         }
 
     }
