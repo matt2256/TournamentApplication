d87b543dd7e25483f5b9ac684b8b3d7fd8303f42
matt2256 <mads@mmm-m.dk>
2020-08-10 14:17:38 +0200

    Connected buttons for creating data with CRUD Methods

--- TournamentApplication/UI/MainWindow.xaml.cs
+++ TournamentApplication/UI/MainWindow.xaml.cs
@@ -39,7 +39,24 @@ namespace UI
         #endregion
 
         #region Player
-        
+        private void BtnPlayerCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Create _create = new Create();
+
+            _create.CreatePeople(TBPlayerFirstName.Text, TBPlayerLastName.Text, TBPlayerInGameName.Text);
+
+            LoadPlayerDtg();
+        }
+
+        private void BtnPlayerEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPlayerDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
 
         public void LoadPlayerDtg()
         {
@@ -51,8 +68,67 @@ namespace UI
 
             PlayerSource.Source = _context.People.Local;
         }
+
+
         #endregion
 
+        #region TeamMembers
+        private void BtnTeamMemberCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Create _Create = new Create();
 
+            _Create.CreateTeamMembers(TBTeamMemberName.Text);
+        }
+
+        private void BtnTeamMemberEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnTeamMemberDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Team
+        private void BtnTeamCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Create _create = new Create();
+
+            _create.CreateTeams(TBTeamTeamID.Text);
+        }
+
+        private void BtnTeamEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnTeamDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Tournament
+        private void BtnTournamentCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Create _create = new Create();
+
+            _create.CreateTournaments(TBTournamentName.Text);
+        }
+
+        private void BtnTournamentEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnTournamentDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
     }
 }
